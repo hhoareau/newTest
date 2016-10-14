70bc07881de5ba0150d03cd118c60b3b2031b1b1
hhoareau <hhoareau@gmail.com>
2016-10-14 09:33:45 +0200

    correction du principe du log

--- src/com/shifumix/Main.java
+++ src/com/shifumix/Main.java
@@ -18,23 +18,19 @@ public class Main {
 
         long start = System.currentTimeMillis();
 
-
         Shifumix a=new Shifumix();
 
         a.setDomain("https://shifumixweb.appspot.com");
         a.openClient("hhoareau@gmail.com", "hh4271!!");
-        log(a.makeEvent("Test", "on va trop s'amuser", "12, rue martel paris"));
-        log(a.makeEvent("Test 2", "on va trop s'amuser", "14, rue de milan paris"));
-        log(a.makeEvent("Test 3", "on va trop s'amuser", "18 rue du chateau paris"));
-        log(a.selEvent("Test 2"));
-        log(a.addSong("cure",1));
-        log(a.addSong("cure",2));
-        log(a.addSong("cure",4));
-        log(a.addSong("cure",5));
-
-
+        a.makeEvent("Test", "on va trop s'amuser", "12, rue martel paris");
+        a.makeEvent("Test 2", "on va trop s'amuser", "14, rue de milan paris");
+        a.makeEvent("Test 3", "on va trop s'amuser", "18 rue du chateau paris");
+        a.selEvent("Test 2");
+        a.addSong("cure",1);
+        a.addSong("cure",2);
+        a.addSong("cure",4);
+        a.addSong("cure",5);
 
         System.out.println("Temps de traitement "+(delay/(1000*60)));
-
     }
 }
